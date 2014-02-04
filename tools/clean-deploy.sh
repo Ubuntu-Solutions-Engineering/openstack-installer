63d92a4d150ecfd6efe14b8cf78c6912fbd5dd16
Tycho Andersen <tycho@tycho.ws>
2014-02-04 04:07:42 -0600

    remove .cloud-installed

--- tools/clean-deploy.sh
+++ tools/clean-deploy.sh
@@ -6,6 +6,7 @@ sudo apt-get -yy purge '.*maas.*' '.*juju.*'
 sudo lxc-kill -n juju-bootstrap
 sudo lxc-destroy -n juju-bootstrap
 sudo service apache2 stop
+sudo rm /etc/.cloud-installed
 
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo you might need to fix your /etc/resolv.conf
