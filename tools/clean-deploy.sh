06bc2e74bcf33d0bbb0c425e14625dcec9ca129e
Tycho Andersen <tycho.andersen@canonical.com>
2014-02-19 16:35:25 +0200

    Clean up networking stuff too

--- tools/clean-deploy.sh
+++ tools/clean-deploy.sh
@@ -10,6 +10,13 @@ sudo lxc-destroy -n juju-bootstrap
 sudo service apache2 stop
 sudo rm /etc/.cloud-installed
 
+# clean up the networking
+sudo rm /etc/network/interfaces.d/cloud-install.cfg
+sudo sed -i -e '/source/d' /etc/network/interfaces
+sudo ifconfig br0 down
+sudo brctl delbr br0
+sudo service networking restart
+
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 echo you might need to fix your /etc/resolv.conf
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
