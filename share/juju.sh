37153ff62f8ffe4db6e671d93fb786b9cc8b1588
Tycho Andersen <tycho@tycho.ws>
2014-02-20 19:07:35 +0200

    Update for maas API fix
    We now have to explicitly state which nodegroup we're in, since they can't use
    127.0.0.1 to autodetect which nodegroup we're in.

--- share/juju.sh
+++ share/juju.sh
@@ -80,6 +80,8 @@ configureManualProvider()
 # TODO break this function into smaller ones
 jujuBootstrap()
 {
+	maas_nodegroup_uuid=$1
+
 	# TODO: uncomment and fix cleanup script when LP 1282657 is fixed
 	# lxc-create -n juju-bootstrap -t ubuntu-cloud -- -r precise
 	sed -e "s/^lxc.network.link.*$/lxc.network.link = br0/" -i \
@@ -89,7 +91,7 @@ jujuBootstrap()
 	    | cut -d " " -f 3)
 	# TODO dynamic architecture selection
 	maas maas nodes new architecture=amd64/generic mac_addresses=$mac \
-	    hostname=juju-bootstrap
+	    hostname=juju-bootstrap nodegroup=$maas_nodegroup_uuid
 	system_id=$(nodeSystemId $mac)
 	wget -O $TMP/maas.creds \
 	    "http://localhost/MAAS/metadata/latest/by-id/$system_id/?op=get_preseed"
