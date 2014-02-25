56638ed162ae482356ef13eee8645f0ef0ce4839
Adam Stokes <adam.stokes@ubuntu.com>
2014-02-25 18:53:20 -0500

    force juju bootstrap into background
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- share/juju.sh
+++ share/juju.sh
@@ -96,7 +96,7 @@ jujuBootstrap()
 	python2 /etc/maas/templates/commissioning-user-data/snippets/maas_signal.py \
 	    --config $TMP/maas.creds OK || true
 
-	(cd "/home/$INSTALL_USER"; sudo -H -u "$INSTALL_USER" juju bootstrap --upload-tools)
+	(cd "/home/$INSTALL_USER"; sudo -H -u "$INSTALL_USER" juju bootstrap --upload-tools &)
 	# TODO There is a delay between adding a machine via the cli, which
 	#      returns instantly, and the juju provisioning worker picking up
 	#      the request and creating the necessary machine placeholder.
