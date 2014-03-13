44b4340a2538b07b4f803f0f593c7739638397d7
Adam Stokes <adam.stokes@ubuntu.com>
2014-03-12 21:42:39 -0400

    still working out threading code
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- tools/juju-api-inspect.py
+++ tools/juju-api-inspect.py
@@ -6,6 +6,7 @@ sys.path.insert(0, '../cloudinstall')
 from cloudinstall.juju.client import JujuClient
 from pprint import pprint
 import json
+import time
 
 JUJU_PASS = os.environ['JUJU_PASS'] if os.environ['JUJU_PASS'] else randomString()
 JUJU_URL = os.environ['JUJU_URL'] if os.environ['JUJU_URL'] else 'wss://juju-bootstrap.master:17070/'
@@ -13,5 +14,5 @@ JUJU_URL = os.environ['JUJU_URL'] if os.environ['JUJU_URL'] else 'wss://juju-boo
 if __name__ == '__main__':
     ws = JujuClient(JUJU_URL)
     ws.login(JUJU_PASS)
-#    ws.call({'Type':'Client', 'Request': 'EnvironmentInfo'})
+    ws.info()
     ws.close()
