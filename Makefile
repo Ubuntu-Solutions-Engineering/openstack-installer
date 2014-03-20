a0eb7615a7e2ea03a6a81e6ed9a3fc80d3fca201
Adam Stokes <adam.stokes@ubuntu.com>
2014-03-20 15:12:13 -0400

    Use kvm for single installer
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@
 # Makefile for cloud-install
 #
 NAME = cloud-installer
-VERSION = $(shell echo `awk  -F "\"" '/^__version__ = "/{print $$2}' cloudinstall/__init__.py`)+git-cb0f8c
+GIT_REV = $(shell git log --oneline -n1| cut -d" " -f1)
+VERSION = $(shell echo `awk  -F "\"" '/^__version__ = "/{print $$2}' cloudinstall/__init__.py`)+git-${GIT_REV}
 TOPDIR = $(shell basename `pwd`)
 
 $(NAME)_$(VERSION).orig.tar.gz: clean
@@ -22,6 +23,10 @@ deb: clean tarball
 	wrap-and-sort
 	@debuild -us -uc -i
 
-.PHONY: status
+current_version:
+	@echo ${GIT_REV}
+
 status:
 	PYTHONPATH=$(shell pwd):$(PYTHONPATH) bin/cloud-status
+
+.PHONY: status current_version deb deb-src clean tarball
--- share/juju.sh
+++ share/juju.sh
@@ -32,49 +32,16 @@ configJujuEnvironment()
 		EOF
 }
 
-configManualEnvironment()
+configLocalEnvironment()
 {
 	cat <<-EOF
-		default: "null"
+		default: local
 
 		environments:
-		  "null":
-		    type: "null"
-		    admin-secret: $1
-		    bootstrap-host: juju-bootstrap
-		    bootstrap-user: ubuntu
-		    storage-auth-key: $2
+		  local:
+		    type: local
+		    container: kvm
 		EOF
-}
-
-configureJuju()
-{
-	mkdir -m 0700 "/home/$INSTALL_USER/.juju"
-	configJujuEnvironment $1 $2 $3 \
-	    > "/home/$INSTALL_USER/.juju/environments.yaml"
-	chmod 0600 "/home/$INSTALL_USER/.juju/environments.yaml"
-	chown -R -- "$INSTALL_USER:$INSTALL_USER" "/home/$INSTALL_USER/.juju"
-}
-
-configureManualDns()
-{
-	cp /etc/resolv.conf /etc/resolv.dnsmasq
-	echo resolv-file=/etc/resolv.dnsmasq > /etc/lxc/dnsmasq.conf
-	echo LXC_DHCP_CONFILE=/etc/lxc/dnsmasq.conf >> /etc/default/lxc-net
-	service lxc-net restart
-	sed -e '/^iface lo inet loopback$/a\
-\	dns-nameservers 10.0.3.1' -i /etc/network/interfaces
-	# lp 1102507
-	ifdown lo; ifup lo
-}
-
-configureManualProvider()
-{
-	mkdir -m 0700 -p "/home/$INSTALL_USER/.juju"
-	configManualEnvironment $1 $2 \
-	    > "/home/$INSTALL_USER/.juju/environments.yaml"
-	chmod 0600 "/home/$INSTALL_USER/.juju/environments.yaml"
-	chown -R -- "$INSTALL_USER:$INSTALL_USER" "/home/$INSTALL_USER/.juju"
 }
 
 # TODO break this function into smaller ones
@@ -104,21 +71,4 @@ jujuBootstrap()
 	    /var/lib/lxc/juju-bootstrap/rootfs/etc/cloud/cloud.cfg.d/91_maas.cfg
 	lxc-start -n juju-bootstrap -d
 	wait $!
-}
-
-bootstrapManualProvider()
-{
-	lxc-create -n juju-bootstrap -t ubuntu-cloud -- -r precise \
-	    -S "/home/$INSTALL_USER/.ssh/id_rsa.pub"
-	lxc-start -n juju-bootstrap -d
-	# wait for container to boot
-	sleep 20
-	(
-		cd "/home/$INSTALL_USER"
-		# lp 1238934
-		sudo -H -u "$INSTALL_USER" ssh -o StrictHostKeyChecking=no \
-		    ubuntu@juju-bootstrap \
-		    'sh -c "sudo add-apt-repository -y cloud-archive:tools && sudo apt-get update"'
-		sudo -H -u "$INSTALL_USER" juju bootstrap --upload-tools
-	)
 }
