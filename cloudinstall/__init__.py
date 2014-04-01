1478323cf056d5d5bfe572a52fa9f31fd95e8d76
Adam Stokes <adam.stokes@ubuntu.com>
2014-04-01 13:42:46 -0400

    Sync upstream version with debian version
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- cloudinstall/__init__.py
+++ cloudinstall/__init__.py
@@ -16,4 +16,4 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.12"
+__version__ = "0.12+git-a5b1413"
