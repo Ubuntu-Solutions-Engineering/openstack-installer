6f1643fb6df82aa63e18352ca4431bea5c3d20e9
Robert Ayres <robert.ayres@canonical.com>
2014-04-01 11:59:25 +0100

    Fix syntax error

--- share/landscape.sh
+++ share/landscape.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function getLandscapeCert() {
+getLandscapeCert() {
 	begin="-----BEGIN CERTIFICATE-----"
 	end="-----END CERTIFICATE-----"
 	cert=$(echo | openssl s_client -connect "$1":443 < /dev/null 2>/dev/null)
