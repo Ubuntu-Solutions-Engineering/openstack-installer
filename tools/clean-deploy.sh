e10b044c7884c24548272c1458ace53a42de43a4
Adam Stokes <adam.stokes@ubuntu.com>
2014-04-09 15:42:06 -0400

    merge single install cleaner into our clean-deploy script
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- tools/clean-deploy.sh
+++ tools/clean-deploy.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
 
-yes | juju destroy-environment --force maas
-yes | juju destroy-environment --force local
+WHAT=$1
 
-rm -r ~/.maascli.db
-rm -rf ~/.juju ~/.cloud-install
+case $WHAT in
+  multi-system)
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    echo Multi install cleansing.
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-sudo apt-get -yy purge '.*maas.*' '.*juju.*'
+    yes | juju destroy-environment --force maas
+    rm -r ~/.maascli.db
+    sudo apt-get -yy purge '.*maas.*'
+    sudo lxc-stop -n juju-bootstrap
+    sudo lxc-destroy -n juju-bootstrap
+    sudo service apache2 stop
+    # clean up the networking
+    sudo rm /etc/network/interfaces.d/cloud-install.cfg
+    sudo sed -i -e '/source/d' /etc/network/interfaces
+    sudo ifconfig br0 down
+    sudo brctl delbr br0
+    sudo service networking restart
+
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    echo you might need to fix your /etc/resolv.conf
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    ;;
+  single-system)
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    echo Single install cleansing.
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    yes | juju destroy-environment --force local
+    ;;
+  *)
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    echo Please run with either single-system or multi-system as an argument.
+    echo Example:
+    echo   ./clean-deploy.sh single-system
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    exit 1
+    ;;
+  esac
 
 # these may or may not be installed, so we list them all individually
-sudo apt-get -yy purge cloud-install-single
-sudo apt-get -yy purge cloud-install-multi
-sudo apt-get -yy purge cloud-install-landscape
-sudo apt-get -yy purge cloud-installer
+sudo apt-get -q -yy purge cloud-install-single
+sudo apt-get -q -yy purge cloud-install-multi
+sudo apt-get -q -yy purge cloud-install-landscape
+sudo apt-get -q -yy purge cloud-installer
 
-sudo lxc-stop -n juju-bootstrap
-sudo lxc-destroy -n juju-bootstrap
-sudo service apache2 stop
-sudo rm /etc/.cloud-installed
+sudo rm -rf ~/.juju ~/.cloud-install || true
+sudo rm /etc/.cloud-installed || true
 
-# clean up the networking
-sudo rm /etc/network/interfaces.d/cloud-install.cfg
-sudo sed -i -e '/source/d' /etc/network/interfaces
-sudo ifconfig br0 down
-sudo brctl delbr br0
-sudo service networking restart
-
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-echo you might need to fix your /etc/resolv.conf
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
