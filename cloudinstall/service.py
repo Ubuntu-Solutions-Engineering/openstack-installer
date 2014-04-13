b5f1b58a183910754a50a6bac9b16707fceef8cd
Adam Stokes <adam.stokes@ubuntu.com>
2014-04-13 18:48:40 -0400

    Add service, units, relations classes
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ cloudinstall/service.py
@@ -0,0 +1,100 @@
+#
+# service.py - Juju Services and Units
+#
+# Copyright 2014 Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+""" Represents a Juju service """
+
+
+class Unit:
+    """ Unit class """
+
+    def __init__(self, unit_name, unit):
+        self.unit_name = unit_name
+        self.unit = unit
+
+    @property
+    def agent_state(self):
+        """ Unit's agent state
+
+        :returns: agent state
+        :rtype: str
+        """
+        return self.unit.get('agent-state', 'unknown')
+
+
+class Relation:
+    """ Relation class """
+
+    def __init__(self, relation_name, relation):
+        self.relation_name = relation_name
+        self.relation = relation
+
+    def charms(self, name):
+        """ Charms for relation
+
+        :param str name: name of relation
+        :returns: list of charms
+        :rtype: list
+        """
+        return self.relation.get(name, [])
+
+
+class Service:
+    """ Service class """
+
+    def __init__(self, service_name, service):
+        self.service_name = service_name
+        self.service = service
+
+    @property
+    def charm(self):
+        """ Charm
+
+        :returns: Charm Path
+        :rtype: str
+        """
+        return self.service.get('charm', '')
+
+    @property
+    def exposed(self):
+        """ Exposed
+
+        :returns: if service is exposed
+        :rtype: bool
+        """
+
+    @property
+    def units(self):
+        """ Service units
+
+        :returns: list associated units for service
+        :rtype: Unit()
+        """
+        for unit_name, unit in self.service.get('units', {}).items():
+            yield Unit(unit_name, unit)
+
+
+    @property
+    def relations(self):
+        """ Service relations
+
+        :returns: list of relations for service
+        :rtype: Relation()
+        """
+        for relation_name, relation in \
+            self.service.get('relations', {}).items():
+            yield Relation(relation_name, relation)
