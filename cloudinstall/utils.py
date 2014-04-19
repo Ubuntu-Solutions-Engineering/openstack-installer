c59b1b990b3c36ebd74577e2ed6cae1b83aa57d6
Adam Stokes <adam.stokes@ubuntu.com>
2014-04-18 20:26:31 -0400

    add module import helpers
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- cloudinstall/utils.py
+++ cloudinstall/utils.py
@@ -23,6 +23,7 @@ import re
 import string
 import random
 import inspect
+import fnmatch
 from time import strftime
 
 # String with number of minutes, or None.
@@ -170,40 +171,42 @@ def time():
     return strftime('%Y-%m-%d %H:%M')
 
 
-def import_module(module_fqname, superclasses=None):
-    """Imports the module module_fqname and returns a list of defined classes
-    from that module. If superclasses is defined then the classes returned will
-    be subclasses of the specified superclass or superclasses. If superclasses
-    is plural it must be a tuple of classes.
+def find(file_pattern, top_dir, max_depth=None, path_pattern=None):
+    """generator function to find files recursively. Usage:
 
-    .. note::
-
-        Taken from: https://github.com/sosreport/sos/blob/master/sos/utilities.py
-
-    :param str module_fqname: module path
-    :param superclasses: subclasses of defined class
+    for filename in find("*.properties", "/var/log/foobar"):
+        print filename
     """
+    if max_depth:
+        base_depth = os.path.dirname(top_dir).count(os.path.sep)
+        max_depth += base_depth
+
+    for path, dirlist, filelist in os.walk(top_dir):
+        if max_depth and path.count(os.path.sep) >= max_depth:
+            del dirlist[:]
+
+        if path_pattern and not fnmatch.fnmatch(path, path_pattern):
+            continue
+
+        for name in fnmatch.filter(filelist, file_pattern):
+            yield os.path.join(path, name)
+
+
+def import_module(module_fqname):
+    """Imports the module module_fqname and returns a list of defined classes
+    from that module."""
     module_name = module_fqname.rpartition(".")[-1]
     module = __import__(module_fqname, globals(), locals(), [module_name])
     modules = [class_ for cname, class_ in
                inspect.getmembers(module, inspect.isclass)
                if class_.__module__ == module_fqname]
-    if superclasses:
-        modules = [m for m in modules if issubclass(m, superclasses)]
-
     return modules
 
 
 class ImporterHelper(object):
-    """ Provides a list of modules that can be imported in a package.
+    """Provides a list of modules that can be imported in a package.
     Importable modules are located along the module __path__ list and modules
-    are files that end in .py.
-
-
-    .. note::
-
-        Taken from: https://github.com/sosreport/sos/blob/master/sos/utilities.py
-    """
+    are files that end in .py."""
 
     def __init__(self, package):
         """package is a package module
@@ -240,7 +243,4 @@ class ImporterHelper(object):
         for path in self.package.__path__:
             if os.path.isdir(path) or path == '':
                 plugins.extend(self._find_plugins_in_dir(path))
-            else:
-                plugins.extend(self._find_plugins_in_zipfile(path))
-
         return plugins
