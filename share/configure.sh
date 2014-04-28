f4bcdd436b9f35064db346c88d159fa6c8248b82
Robert Ayres <robert.ayres@canonical.com>
2014-04-28 11:57:43 +0100

    Add multi install system requirements check

--- share/configure.sh
+++ share/configure.sh
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+MULTI_DISK_MIN=10
+MULTI_RAM_MIN=2
 SINGLE_DISK_MIN=20
 SINGLE_RAM_MIN=8
 
@@ -36,6 +38,14 @@ checkMemory()
 		fail=$((fail + 1))
 		text="$text[!!] You need at least ${1}GB of memory\n\n"
 	fi
+}
+
+checkMultiInstall()
+{
+	fail=0
+	text=""
+	checkMemory $MULTI_RAM_MIN
+	checkDiskSpace $MULTI_DISK_MIN
 }
 
 checkSingleInstall()
@@ -70,6 +80,13 @@ configureInstall()
 			esac
 			;;
 		10)
+			checkMultiInstall
+			if [ $fail -ne 0 ] && ! requirementsError; then
+				popState; continue
+			fi
+			state=11; continue
+			;;
+		11)
 			interfaces=$(getInterfaces)
 			interfaces_count=$(echo "$interfaces" | wc -w)
 			if [ $interfaces_count -ge 2 ]; then
@@ -82,7 +99,7 @@ configureInstall()
 				state=$next_state; continue
 			fi
 			;;
-		11)
+		12)
 			bridge_interface=""
 			if [ $interfaces_count -ge 2 ]; then
 				if dialogYesNo "Bridge interface?" Yes No "Sometimes it is useful to run MaaS on its own network. If you are running MaaS on its own network and would like to bridge this network to the outside world, please indicate so." 10 60; then
@@ -91,7 +108,7 @@ configureInstall()
 			fi
 			state=$next_state; continue
 			;;
-		12)
+		13)
 			network=$(ipNetwork $interface)
 			address=$(ipAddress $interface)
 			if [ -n "$bridge_interface" ]; then
@@ -102,7 +119,7 @@ configureInstall()
 			fi
 			state=$next_state; continue
 			;;
-		13)
+		14)
 			dhcp_range=$(dialogInput "IP address range (<ip addr low>-<ip addr high>):" "IP address range for DHCP leases.\nNew nodes will be assigned addresses from this pool." 10 60 "$dhcp_range")
 			if [ -z "$dhcp_range" ]; then
 				popState; continue
@@ -156,6 +173,6 @@ pushState()
 requirementsError()
 {
 	dialogYesNo "[!] System requirements not met" Continue Cancel \
-	    "${text}Select Continue to proceed regardless" $((8 + (2 * fail))) \
-	    60
+	    "The following system requirements are not met for $install_type install:\n\n${text}Select Continue to proceed regardless" \
+	    $((11 + (2 * fail))) 60
 }
