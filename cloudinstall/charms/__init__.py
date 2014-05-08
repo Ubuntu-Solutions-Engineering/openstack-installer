3fb04ccfcc8eac6c2dab62e31f2b966741413452
Michael McCracken <mike.mccracken@canonical.com>
2014-05-08 12:04:54 -0700

    Refactor state handling code.
    Separate juju and maas state throughout the code to make the code handling state more explicit.
    
    Rename pegasus.parse_state to update_machine_info and do not build a list of [Machine, MaasMachine] which is not used anywhere.
    
    Update some documentation strings
    
    Minor pep8 line-length and whitespace fixes
    
    In gui.py, ControllerOverlay: promote update_and_redraw to a member function for readability.
    
    Update test_pegasus to match api changes, tests still need much work.

--- cloudinstall/charms/__init__.py
+++ cloudinstall/charms/__init__.py
@@ -37,7 +37,7 @@ class CharmBase:
     configfile = expanduser("~/.cloud-install/charmconf.yaml")
     deploy_priority = sys.maxsize
 
-    def __init__(self, state=None, machine=None):
+    def __init__(self, juju_state=None, machine=None):
         """ initialize
 
         :param state: :class:JujuState
@@ -45,7 +45,7 @@ class CharmBase:
         """
         self.charm_path = None
         self.exposed = False
-        self.state = state
+        self.juju_state = juju_state
         self.machine = machine
         self.client = JujuClient()
 
@@ -113,7 +113,7 @@ class CharmBase:
         Override in charm specific.
         """
         if len(self.related) > 0:
-            services = self.state[1].service(self.charm_name)
+            services = self.juju_state.service(self.charm_name)
             for charm in self.related:
                 try:
                     if not self.is_related(charm, services.relations) \
