bdbf91b34d7409a092f4e572845c92a82ee1e8c1
Adam Stokes <adam.stokes@ubuntu.com>
2015-01-22 20:13:25 -0500

    fix call to status_info_message
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- cloudinstall/single_install.py
+++ cloudinstall/single_install.py
@@ -302,7 +302,7 @@ class SingleInstall:
 
         # start the party
         cloud_status_bin = ['openstack-status']
-        self.display_controller.info_message("Bootstrapping Juju")
+        self.display_controller.status_info_message("Bootstrapping Juju")
         self.tasker.start_task("Bootstrapping Juju")
         utils.container_run(self.container_name,
                             "JUJU_HOME=~/.cloud-install juju bootstrap",
