197740718b66132a6d22689f4da7256add5d01c2
Michael McCracken <mike.mccracken@canonical.com>
2015-04-13 19:34:53 -0400

    add logger

--- cloudinstall/placement/ui/services_list.py
+++ cloudinstall/placement/ui/services_list.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+
 from urwid import (AttrMap, Divider, Padding, Pile, Text,
                    WidgetWrap)
 
@@ -20,6 +22,7 @@ from cloudinstall.maas import satisfies
 from cloudinstall.state import CharmState
 from cloudinstall.placement.ui.service_widget import ServiceWidget
 
+log = logging.getLogger('cloudinstall.placement.ui')
 
 class ServicesList(WidgetWrap):
 
