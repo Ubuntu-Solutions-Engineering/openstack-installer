96fba9ede2a841f908b8ee633695454365571098
Adam Stokes <adam.stokes@ubuntu.com>
2015-06-02 17:41:53 -0400

    update Copyright

--- cloudinstall/utils.py
+++ cloudinstall/utils.py
@@ -1,7 +1,5 @@
 #
-# utils.py - Helper utilies for cloud installer
-#
-# Copyright 2014 Canonical, Ltd.
+# Copyright 2014, 2015 Canonical, Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
