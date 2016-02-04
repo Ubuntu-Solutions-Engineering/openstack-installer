bee3a946a8f265c2f5e3e840478775cfc2b34cad
Adam Stokes <adam.stokes@ubuntu.com>
2016-02-04 15:46:56 -0500

    update integration test
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- integration/lds-runner.sh
+++ integration/lds-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 OSI_TESTRUNNER_ID=battlemidgetjenkins sudo -E openstack-install -c \
-                 $WORKSPACE/openstack-tests/profiles/lds.yaml \
+                 $OPENSTACK_CONFIG \
                  --constraints "mem=16 tags=autopilot"
 
 py.test-3 $WORKSPACE/integration/tests/test_autopilot.py
