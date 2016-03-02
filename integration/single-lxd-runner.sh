f80db018c2d56a9831b92bbd1b38a5503be0ea4e
Adam Stokes <adam.stokes@ubuntu.com>
2016-03-01 19:27:35 -0500

    support xenial
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- integration/single-lxd-runner.sh
+++ integration/single-lxd-runner.sh
@@ -5,9 +5,14 @@ if [ $? -eq 0 ]; then
     sudo add-apt-repository -y ppa:ubuntu-lxc/lxd-stable
     sudo apt-get update
     sudo apt-get install -qyf lxd
+    sudo su ubuntu -c "lxc list"
+    sudo su ubuntu -c "lxd-images import ubuntu --alias ubuntu"
+else
+    sudo su ubuntu -c "lxc list"
+    sudo su ubuntu -c "lxd-images import ubuntu xenial amd64 --alias ubuntu"
 fi
 
-sudo su ubuntu -c "lxc list"
-sudo su ubuntu -c "lxd-images import ubuntu --alias ubuntu"
+
+
 
 OSI_TESTRUNNER_ID=battlemidgetjenkins sudo -E openstack-install -c $OPENSTACK_CONFIG
